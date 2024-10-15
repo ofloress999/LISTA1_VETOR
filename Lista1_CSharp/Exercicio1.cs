e2ff22ece36b8e0fae23310de00c8dac71799508
ladoo <ladoo@DESKTOP-1OG4B3L>
2024-10-15 20:19:36 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1_CSharp/Exercicio1.cs
@@ -0,0 +1,14 @@
+﻿using System; //Adiciona a biblioteca System
+using bibliotecaArray;
+
+
+namespace Lista1_CSharp;
+public class Exercicio1
+{
+    private static void Main()
+    {
+        int 
+
+    }
+
+}
---
+++ Lista1_CSharp/bibliotecaArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace bibliotecaArray
+{
+
+    public static class Matematica
+    {
+
+        // Função somar
+        public static int somar(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        // Função subtrair
+        public static int subtrair (int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        // Função multiplicar
+        public static int multiplicar(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        // Função dividir
+        public static int dividir(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+    }
+
+    public static class Array
+    {
+
+    }
+   
+}
